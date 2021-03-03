0e2d94989a309a1bf5c0ee9fbe13cc8b30ef587b
dylan <dylannelson58@gmail.com>
2021-03-03 08:49:37 -0700

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class WorkingWithFilesForm
+    Private Sub WorkingWithFilesForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        FileDemoRead()
+    End Sub
+
+    'Private Sub FileDemo()
+    '    FileOpen(1, "FileDemo.txt", OpenMode.Append)
+
+    '    WriteLine(1, "Hello")
+
+    '    FileClose(1)
+
+    'End Sub
+
+    Sub FileDemoRead()
+        Try
+            FileOpen(1, "someTextFile.txt", OpenMode.Input)
+
+        Catch ex As Exception
+
+        End Try
+
+        FileClose(1)
+    End Sub
+End Class
